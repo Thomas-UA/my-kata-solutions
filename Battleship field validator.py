56f36468b1c58030f1a1b42ac78169edfe1db24d
Thomas-UA <faustpear@gmail.com>
2022-05-19 22:51:46 +0300

    added 2 dicts and first 'for' cycle

--- Battleship field validator.py
+++ Battleship field validator.py
@@ -13,7 +13,31 @@ def validate_battlefield(field):
         "submarines": 0
     }
 
-    return True if ships == ships_true else False
+    coordinats = {
+        "0": [],
+        "1": [],
+        "2": [],
+        "3": [],
+        "4": [],
+        "5": [],
+        "6": [],
+        "7": [],
+        "8": [],
+        "9": []
+    }
+
+    for horizontal_list in range(len(field)):
+        # print(horizontal_list)
+        for area in range(len(field[horizontal_list])):
+            # print(area)
+            if field[horizontal_list][area] == 1:
+                coordinats[str(horizontal_list)].append(area)
+
+    
+
+
+    return coordinats
+    # return True if ships == ships_true else False
 
 battleField =   [[1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
                  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
